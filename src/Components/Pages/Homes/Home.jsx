9cdc240a50d3639272e1b8b9f24aa6163056fc09
che <mitsubhra@gmail.com>
2022-11-08 13:11:50 +0530

    Search Complete

--- src/Components/Pages/Homes/Home.jsx
+++ src/Components/Pages/Homes/Home.jsx
@@ -3,57 +3,75 @@ import TextField from '@mui/material/TextField'
 import Autocomplete from '@mui/material/Autocomplete'
 import ImageList from '@mui/material/ImageList'
 import ImageListItem from '@mui/material/ImageListItem'
+import Button from '@mui/material/Button'
 import './Home.css'
 import axios from 'axios';
-import { createClient } from 'pexels';
+import { useEffect } from 'react'
+
 
 const Home = () => {
 
-    const [search, setSearch] = useState('');
+
+    const options = ['Nature', 'Tigers', 'People']
+
+    const [value, setValue] = useState('');
+    const [inputValue, setInputValue] = useState('');
     const [photos, setPhotos] = useState('');
 
-
-    const top100Image = ['Nature', 'Tigers', 'Claas Jaguar chopper standing on meadow']
-
-    function handleInput(event) {
-
-        const search = event.target.value;
-        console.log(search);
-        setSearch(search);
-
-
-        const url = "https://api.pexels.com/v1/search?query=" + search;
+    useEffect(() => {
+        const url = `https://api.pexels.com/v1/search?query=nature`;
         const access_token = '563492ad6f9170000100000133d33750d93d4701ade76b3513d57544';
         axios.get(url, {
             headers: {
                 'Authorization': `${access_token}`
             }
         }).then(response => {
-            console.log(response)
-            setPhotos(response.photos)
+            setPhotos(response.data.photos)
         })
+    }, [])
 
+    function handleSearch() {
+        const url = "https://api.pexels.com/v1/search?query=" + value;
+        const access_token = '563492ad6f9170000100000133d33750d93d4701ade76b3513d57544';
+        axios.get(url, {
+            headers: {
+                'Authorization': `${access_token}`
+            }
+        }).then(response => {
+            setPhotos(response.data.photos)
+        })
     }
 
     return (
         <div className='main-container'>
             <div className='main-row'>
                 <div className='main-col'>
+
                     <Autocomplete
-                        disablePortal
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                        inputValue={inputValue}
+                        onInputChange={(event, newInputValue) => {
+                            setInputValue(newInputValue);
+                        }}
+
                         id="combo-box-demo"
-                        className='auto'
-                        defaultValue={[top100Image[0]]}
-                        value={top100Image[0]}
-                        options={top100Image}
-                        onChange={handleInput}
+                        options={options}
+                        sx={{ width: 300 }}
                         renderInput={(params) => <TextField {...params} label="Image" />}
                     />
+                    <Button 
+                    varient="contained"
+                    onClick={handleSearch}>Search</Button>
+
                 </div>
             </div>
+
             <div className='imagelist-row'>
                 <div className='imagelist-col'>
-                    <ImageList>
+                    {/* <ImageList>
                         <ImageListItem>
 
                             <img
@@ -63,7 +81,18 @@ const Home = () => {
                                 loading="lazy"
                             />
                         </ImageListItem>
-                    </ImageList>
+                    </ImageList> */}
+                    <div className='img-row'>
+                        {
+                            photos && photos.map((data, index) => {
+                                return (
+                                    <>
+                                        <img key={index} src={data.src.small} alt="" />
+                                    </>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
